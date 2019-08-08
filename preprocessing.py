ceeadfd695c9507f0c3a45b08196a17990887b2f
Ashish Jain <ashish.jain@mobiledgex.com>
2019-08-08 11:03:04 +0530

    Another implementation of cutout

--- preprocessing.py
+++ preprocessing.py
@@ -48,3 +48,34 @@ def random_pad_crop(img, pad_size):
     x = np.random.randint(0, width - dx + 1)
     y = np.random.randint(0, height - dy + 1)
     return img[y:(y+dy), x:(x+dx), :]
+
+def cutout(img, max_holes=1, max_length=4):
+    height = img.shape[1]
+    width = img.shape[2]
+
+    # normalize before adding the mask
+    mean = img.mean(keepdims=True)
+    img -= mean
+
+    mask = np.ones((height, width), np.float32)
+    nb_holes = np.random.randint(0, max_holes + 1)
+
+    for i in range(nb_holes):
+        y = np.random.randint(height)
+        x = np.random.randint(width)
+        length = 4 * np.random.randint(1, max_length + 1)
+
+        y1 = np.clip(y - length // 2, 0, height)
+        y2 = np.clip(y + length // 2, 0, height)
+        x1 = np.clip(x - length // 2, 0, width)
+        x2 = np.clip(x + length // 2, 0, width)
+
+        mask[y1: y2, x1: x2] = 0.
+
+    # apply mask
+    img = img * mask
+
+    # denormalize
+    img += mean
+
+    return img
